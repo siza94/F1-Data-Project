64de6a0f4018fb91b739559394c05dcb8f61c105
Sithsaba Zantsi <103945261+siza94@users.noreply.github.com>
2025-07-16 11:11:47 +0200

    Add files via upload

---
+++ Formula1/set-up/3.mount_adls_containers_for_project.py
@@ -0,0 +1,65 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Mount Azure Data Lake Containers for the Project
+# MAGIC #### Steps to follow
+# MAGIC 1. Get client_id, tenant_id and client_secret from key vault
+# MAGIC 2. Set Spark Config with App/ Client Id, Directory/ Tenant Id & Secret
+# MAGIC 3. Call file system utlity mount to mount the storage
+# MAGIC 4. Explore other file system utlities related to mount (list all mounts, unmount)
+
+# COMMAND ----------
+
+def mount_adls(storage_account_name, container_name):
+    # Get secret keys from Key Vault
+    client_id = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-client-id')
+    tenant_id = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-tenant-id')
+    client_secret = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-secret-id')
+
+    # Set Spark configurations
+    configs = {"fs.azure.account.auth.type": "OAuth",
+          "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+          "fs.azure.account.oauth2.client.id": client_id,
+          "fs.azure.account.oauth2.client.secret": client_secret,
+          "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
+    
+    if any(mount.mountPoint == f"/mnt/{storage_account_name}/{container_name}" for mount in dbutils.fs.mounts()):
+        dbutils.fs.unmount(f"/mnt/{storage_account_name}/{container_name}")
+
+    # Mount storage account container
+    dbutils.fs.mount(
+        source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
+        mount_point = f"/mnt/{storage_account_name}/{container_name}",
+        extra_configs = configs)
+    
+    display(dbutils.fs.mounts())
+
+# COMMAND ----------
+
+mount_adls("formula1sithsaba", "demo")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Mount Bronze Container
+
+# COMMAND ----------
+
+mount_adls("formula1sithsaba", "bronze")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Mount Silver **Container**
+
+# COMMAND ----------
+
+mount_adls("formula1sithsaba", "silver")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Mount Gold Container
+
+# COMMAND ----------
+
+mount_adls("formula1sithsaba", "gold")
