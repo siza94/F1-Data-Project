215cc0b5c5a0cbe033471c558f960fc4231c75be
Sithsaba Zantsi <103945261+siza94@users.noreply.github.com>
2025-07-16 10:40:48 +0200

    Create configuration.py

---
+++ Formula1/includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+bronze_folder_path = "/mnt/formula1sithsaba/bronze"
+silver_folder_path = "/mnt/formula1sithsaba/silver"
+gold_folder_path = "/mnt/formula1sithsaba/gold"
