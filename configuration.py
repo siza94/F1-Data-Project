8d4ab88377fe44cfadd6efd023197a4aeee62f10
Sithsaba Zantsi <103945261+siza94@users.noreply.github.com>
2025-07-16 10:37:29 +0200

    Delete configuration.py

--- configuration.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-bronze_folder_path = "/mnt/formula1sithsaba/bronze"
-silver_folder_path = "/mnt/formula1sithsaba/silver"
-gold_folder_path = "/mnt/formula1sithsaba/gold"
